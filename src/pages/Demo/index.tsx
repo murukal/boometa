2d3f33b98316823bd1b850dd18384c9ffcdeb421
murukal <jokerxxx222@126.com>
2022-04-17 22:15:02 +0800

    feat: ts

--- src/pages/Demo/index.tsx
+++ src/pages/Demo/index.tsx
@@ -1,11 +1,7 @@
-import { BeeDown } from 'beedown'
+import BeeDown from 'beedown'
 
 const Demo = () => {
-  import('beedown').then((s) => {
-    console.log('s====', s)
-  })
-
-  return <BeeDown />
+  return <BeeDown></BeeDown>
 }
 
 export default Demo
